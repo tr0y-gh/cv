788b3c51e413d23a87018e6d9d8ea61ca647f571
Troy <>
2026-05-18 10:57:17 +0200

    feat: string format util

--- src/utils.js
+++ src/utils.js
@@ -2,3 +2,6 @@ export const $ = {
   one: (...selectors) => document.querySelector(...selectors),
   all: (...selectors) => document.querySelectorAll(...selectors),
 }
+export function format(str, ...args) {
+  return str.replace(/%s/g, () => args.shift())
+}
